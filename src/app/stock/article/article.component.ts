3e471a8ba3d557c041221d4a43a12f2914af6fe5
EL OUAHMANI <younes.el-ouahmani@capgemini.com>
2019-03-06 19:14:53 +0100

    Utilisation directive NgFor

--- src/app/stock/article/article.component.ts
+++ src/app/stock/article/article.component.ts
@@ -8,23 +8,20 @@ import { Stock } from '../../model/stock';
 })
 export class ArticleComponent implements OnInit {
 
-  public stock : Stock;
-  public stockStyles;
+  public stocks : Array<Stock>;
   constructor() { }
 
   ngOnInit() {
-    this.stock = new Stock('Test Stock Company', 'TSC', 85, 80);
-    let diff = (this.stock.price / this.stock.previousPrice) - 1;
-    let largeChange = Math.abs(diff) > 0.01;
-    this.stockStyles = {
-        "color": this.stock.isPositiveChange() ? "green" : "red",
-        "font-size": largeChange ? "1.2em" : "0.8em"
-    };
+    this.stocks = [
+        new Stock('Test Stock Company', 'TSC', 85, 80),
+        new Stock('Second Stock Company', 'SSC', 10, 20),
+        new Stock('Last Stock Company', 'LSC', 876, 765)
+    ];
   }
     
-  toggleFavorite(event) {
+  toggleFavorite(event,index) {
       console.log('We are toggling the favorite state for this stock', event);
-      this.stock.favorite = !this.stock.favorite;
+      this.stocks[index].favorite = !this.stocks[index].favorite;
   }
 
 }
