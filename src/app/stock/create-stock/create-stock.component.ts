0afb6dcedb4b2efd0b93532372e8fe4ae58ffecc
EL OUAHMANI <younes.el-ouahmani@capgemini.com>
2019-03-08 10:55:49 +0100

    Utilisation des FormBuilder - Template-driven forms

--- src/app/stock/create-stock/create-stock.component.ts
+++ src/app/stock/create-stock/create-stock.component.ts
@@ -1,17 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Validators, FormBuilder } from '@angular/forms';
 @Component({
     selector: 'app-create-stock',
     templateUrl: './create-stock.component.html',
     styleUrls: ['./create-stock.component.css']
 })
 export class CreateStockComponent {
-    public stockForm: FormGroup = new FormGroup({
-        name: new FormControl(null, Validators.required),
-        code: new FormControl(null, [Validators.required, Validators.minLength(2)]),
-        price: new FormControl(0, [Validators.required, Validators.min(0)])
-    });
-    constructor() {}
+    public stockForm: FormGroup;
+    constructor(private fb: FormBuilder) {
+        this.createForm();
+    }
+    createForm() {
+        this.stockForm = this.fb.group({
+            name: [null, Validators.required],
+            code: [null, [Validators.required, Validators.minLength(2)]],
+            price: [0, [Validators.required, Validators.min(0)]]
+        });
+    }
     onSubmit() {
         console.log('Stock Form Value', this.stockForm.value);
     }
