ca506c6e97a168820b4bb3c0d3011df2255dcfe4
EL OUAHMANI <younes.el-ouahmani@capgemini.com>
2019-03-08 10:46:40 +0100

    Utilisation des FormGroups - Reactive forms

--- src/app/stock/create-stock/create-stock.component.ts
+++ src/app/stock/create-stock/create-stock.component.ts
@@ -1,14 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 @Component({
-  selector: 'app-create-stock',
-  templateUrl: './create-stock.component.html',
-  styleUrls: ['./create-stock.component.css']
+    selector: 'app-create-stock',
+    templateUrl: './create-stock.component.html',
+    styleUrls: ['./create-stock.component.css']
 })
 export class CreateStockComponent {
-    public nameControl = new FormControl();
+    public stockForm: FormGroup = new FormGroup({
+        name: new FormControl(null, Validators.required),
+        code: new FormControl(null, [Validators.required, Validators.minLength(2)]),
+        price: new FormControl(0, [Validators.required, Validators.min(0)])
+    });
     constructor() {}
     onSubmit() {
-        console.log('Name Control Value', this.nameControl.value);
+        console.log('Stock Form Value', this.stockForm.value);
     }
 }
