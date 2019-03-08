be321a0fc370f251d76961e78c08df6a2b4d87f3
EL OUAHMANI <younes.el-ouahmani@capgemini.com>
2019-03-08 12:03:15 +0100

    Form and Data Model - Reactive forms

--- src/app/stock/create-stock/create-stock.component.ts
+++ src/app/stock/create-stock/create-stock.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { Validators, FormBuilder } from '@angular/forms';
+import { Stock } from '../../model/stock'
+let counter = 1;
 @Component({
     selector: 'app-create-stock',
     templateUrl: './create-stock.component.html',
     styleUrls: ['./create-stock.component.css']
 })
 export class CreateStockComponent {
+    private stock: Stock;
     public stockForm: FormGroup;
     constructor(private fb: FormBuilder) {
         this.createForm();
+        this.stock = new Stock('Test ' + counter++, 'TST', 20, 10);
     }
     createForm() {
         this.stockForm = this.fb.group({
@@ -18,9 +22,22 @@ export class CreateStockComponent {
             price: [0, [Validators.required, Validators.min(0)]]
         });
     }
-    onSubmit() {
-        console.log('Stock Form Value', this.stockForm.value);
+    loadStockFromServer() {
+        this.stock = new Stock('Test ' + counter++, 'TST', 20, 10);
+        let stockFormModel = Object.assign({}, this.stock);
+        delete stockFormModel.previousPrice;
+        delete stockFormModel.favorite;
+        this.stockForm.setValue(stockFormModel);
     }
-    get name() { return this.stockForm.get('name'); }
-
+    patchStockForm() {
+        this.stock = new Stock(`Test ${counter++}`, 'TST', 20, 10);
+        this.stockForm.patchValue(this.stock);
+    }
+    resetForm() {
+        this.stockForm.reset();
+    }
+    onSubmit() {
+        this.stock = Object.assign({}, this.stockForm.value);
+        console.log('Saving stock', this.stock);
+    }
 }
