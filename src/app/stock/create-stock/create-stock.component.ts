f63789834f240c9bfc2ae819cee5a102e635deae
EL OUAHMANI <younes.el-ouahmani@capgemini.com>
2019-03-08 10:10:48 +0100

    Formulaires - Controle validation - Messages d'erreurs

--- src/app/stock/create-stock/create-stock.component.ts
+++ src/app/stock/create-stock/create-stock.component.ts
@@ -12,7 +12,7 @@ export class CreateStockComponent {
   public exchanges = ['NYSE', 'NASDAQ', 'OTHER'];
     
   constructor() {
-      this.stock = new Stock('test', '', 0, 0, 'NASDAQ');
+      this.stock = new Stock('', '', 0, 0, 'NASDAQ');
   }
     
   setStockPrice(price) {
@@ -20,7 +20,12 @@ export class CreateStockComponent {
     this.stock.previousPrice = price;
   }
 
-  createStock() {
-      console.log('Creating stock ', this.stock);
+  createStock(stockForm) {
+    console.log('Stock form', stockForm);
+    if (stockForm.valid) {
+        console.log('Creating stock ', this.stock);
+    } else {
+        console.error('Stock form is in an invalid state');
+    }
   }
 }
