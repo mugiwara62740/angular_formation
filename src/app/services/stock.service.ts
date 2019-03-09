8258f9d68a9e2a6a47a3128bab6d51872092d982
EL OUAHMANI <younes.el-ouahmani@capgemini.com>
2019-03-09 10:29:46 +0100

    Making HTTP GET/POST Calls

--- src/app/services/stock.service.ts
+++ src/app/services/stock.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 import { Observable } from 'rxjs';
 import { throwError as ObservableThrow} from "rxjs"
@@ -8,31 +9,22 @@ import { Stock } from '../model/stock';
 @Injectable()
 export class StockService {
 
-  private stocks: Stock[];
-  constructor() {
-    this.stocks = [
-      new Stock('Test Stock Company', 'TSC', 85, 80, 'NASDAQ'),
-      new Stock('Second Stock Company', 'SSC', 10, 20, 'NSE'),
-      new Stock('Last Stock Company', 'LSC', 876, 765, 'NYSE')
-    ];
-   }
-
-  getStocks() : Observable<Stock[]> {
-    return ObservableOf(this.stocks);
-  }
-
-  createStock(stock: Stock): Observable<any> {
-    let foundStock = this.stocks.find(each => each.code === stock.code);
-    if (foundStock) {
-      return ObservableThrow({msg: 'Stock with code ' + stock.code + ' already exists'});
+    private stocks: Stock[];
+    constructor(private http : HttpClient) {
     }
-    this.stocks.push(stock);
-    return ObservableOf({msg: 'Stock with code ' + stock.code + ' successfully created'});;
-  }
 
-  toggleFavorite(stock: Stock): Observable<Stock> {
-    let foundStock = this.stocks.find(each => each.code === stock.code);
-    foundStock.favorite = !foundStock.favorite;
-    return ObservableOf(foundStock);
-  }
+    getStocks() : Observable<Stock[]> {
+        return this.http.get<Stock[]>('/api/stock');
+    }
+
+    createStock(stock: Stock): Observable<any> {
+        return this.http.post('/api/stock', stock)
+    }
+
+    toggleFavorite(stock: Stock): Observable<Stock> {
+        return this.http.patch<Stock>('/api/stock/' + stock.code,
+                                      {
+            favorite: !stock.favorite
+        });
+    }
 }
--- src/app/stock/stock-list/stock-list.component.ts
+++ src/app/stock/stock-list/stock-list.component.ts
@@ -2,28 +2,15 @@ import { Component, OnInit } from '@angular/core';
 import { StockService } from '../../services/stock.service';
 import { Stock } from '../../model/stock';
 import { Observable } from 'rxjs';
-
 @Component({
-  selector: 'app-stock-list',
-  templateUrl: './stock-list.component.html',
-  styleUrls: ['./stock-list.component.css']
+    selector: 'app-stock-list',
+    templateUrl: './stock-list.component.html',
+    styleUrls: ['./stock-list.component.css']
 })
 export class StockListComponent implements OnInit {
-
-  public stocks: Stock[];
-  public stocks$: Observable<Stock[]>;
-  constructor(private stockService: StockService) { }
-
-  ngOnInit() {
-    /**this.stockService.getStocks()
-        .subscribe(stocks => {
-          this.stocks = stocks;
-    });*/
-    this.stocks$ = this.stockService.getStocks();
-  }
-
-  onToggleFavorite(stock: Stock) {
-    console.log('Favorite for stock ', stock, ' was triggered');
-    this.stockService.toggleFavorite(stock);
-  }
+    public stocks$: Observable<Stock[]>;
+    constructor(private stockService: StockService) { }
+    ngOnInit() {
+        this.stocks$ = this.stockService.getStocks();
+    }
 }
