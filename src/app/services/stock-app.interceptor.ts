05d33477ec6b33f2e1da65a163230f612177c809
EL OUAHMANI <younes.el-ouahmani@capgemini.com>
2019-03-10 10:49:05 +0100

    Interceptors - manipulation de la request (clonage)

--- src/app/services/stock-app.interceptor.ts
+++ src/app/services/stock-app.interceptor.ts
@@ -3,15 +3,46 @@ import {HttpEvent, HttpInterceptor, HttpResponse} from '@angular/common/http';
 import {HttpHandler, HttpRequest, HttpErrorResponse} from '@angular/common/http'
 
 import {Observable} from 'rxjs';
+import { tap } from 'rxjs/operators';
+
 
 import { AuthService } from './auth.service';
 
 @Injectable()
 export class StockAppInterceptor implements HttpInterceptor {
-    constructor() {}
-    intercept(req: HttpRequest<any>, next: HttpHandler):
-    Observable<HttpEvent<any>> {
-        console.log('Making a request to ', req.url);
-        return next.handle(req);
+
+    constructor(private authService: AuthService) {}
+
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        if (this.authService.authToken) {
+            const authReq = req.clone({
+                headers: req.headers.set(
+                    'Authorization',
+                    this.authService.authToken
+                )
+            });
+            console.log('Making an authorized request');
+            req = authReq;
+        }
+        return next.handle(req)
+            .pipe(
+            tap(event => this.handleResponse(req, event),
+                error => this.handleError(req, error)));
+    }
+
+
+    handleResponse(req: HttpRequest<any>, event) {
+        console.log('Handling response for ', req.url, event);
+        if (event instanceof HttpResponse) {
+            console.log('Request for ', req.url,
+                        ' Response Status ', event.status,
+                        ' With body ', event.body);
+        }
+    }
+
+    handleError(req: HttpRequest<any>, event) {
+        console.error('Request for ', req.url,
+                      ' Response Status ', event.status,
+                      ' With error ', event.error);
     }
 }
